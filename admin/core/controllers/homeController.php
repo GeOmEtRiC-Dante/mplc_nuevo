f1d4b9774d419e16fc05d61c87a569343423d3e9
Dante <cibertupi0@hotmail.es>
2016-08-29 22:16:02 -0500

    xdd

--- admin/core/controllers/homeController.php
+++ admin/core/controllers/homeController.php
@@ -3,16 +3,9 @@
 class homeController extends Controllers {
 
   public function __construct() {
-    parent::__construct(false, true);
-
-    $d = new Dashboard;
-
-    echo $this->template->render('home/home', array(
-      'hoteles' => $d->get_hotels(),
-      'visitas' => $d->get_visitas(),
-      'clientes' => $d->get_clients(),
-      'divisas' => $d->get_divisas_graf()
-    ));
+    parent::__construct(true);
+    
+    echo $this->template->render('home/home');
   }
 
 }
--- admin/core/controllers/loginController.php
+++ admin/core/controllers/loginController.php
@@ -4,6 +4,7 @@ class loginController extends Controllers {
 
   public function __construct() {
     parent::__construct(false,true);
+    
     echo $this->template->render('login/login');
   }
 
